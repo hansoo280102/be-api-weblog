c435fac9daf642f4ed609676305a1030eed323be
hansoo280102 <tantran012080@gmail.com>
2024-10-01 20:10:36 +0700

    add middleware and a function to handle  errors

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSChema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "cencor"], // Các giá trị có thể nhận của trường role
+      default: "user", // Giá trị mặc định khi không chỉ định role
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSChema);
+
+export default User;
