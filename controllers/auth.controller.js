227b7ef4ccdf49a8b21becae7629f536a2a20d66
hansoo280102 <tantran012080@gmail.com>
2024-10-01 21:52:28 +0700

    thêm thông báo lỗi

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,8 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+  // Kiểm tra xem tất cả các trường có được điền hay không
   if (
     !username ||
     !email ||
@@ -12,20 +14,36 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-
-  const newUser = new User({
-    username,
-    email,
-    password: hashedPassword,
-  });
   try {
+    // Kiểm tra xem username đã tồn tại hay chưa
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return next(errorHandler(400, "Username already exists")); // Trả về lỗi nếu username đã tồn tại
+    }
+
+    const existngEmail = await User.findOne({ email });
+    if (existngEmail) {
+      return next(errorHandler(400, "Email have been used")); // Trả về lỗi nếu username đã tồn tại
+    }
+
+    // Mã hóa password
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    // Tạo người dùng mới
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Lưu người dùng vào cơ sở dữ liệu
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    next(error);
+    // Xử lý lỗi tại đây
+    next(error); // Nếu có lỗi, chuyển đến middleware xử lý lỗi
   }
 };
