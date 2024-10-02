d71eeddf92ea3b832cb16da6fddb3a1e4147971a
hansoo280102 <tantran012080@gmail.com>
2024-10-02 22:49:17 +0700

    create sign in api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+//sign up a user
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -45,5 +47,42 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     // Xử lý lỗi tại đây
     next(error); // Nếu có lỗi, chuyển đến middleware xử lý lỗi
+  }
+};
+
+//sign in a user
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(401, "Invalid email or password"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid email or password"));
+    }
+    // Tạo token JWT
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    // Gửi phản hồi thành công với token và thông báo
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true }) // Lưu token vào cookie với tùy chọn httpOnly
+      .json(rest); // Trả về JSON
+  } catch (error) {
+    next(error);
   }
 };
