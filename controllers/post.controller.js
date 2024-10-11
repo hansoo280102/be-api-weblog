0589ece3ca27d74b1bb584f9e97bded342b5e77d
hansoo280102 <tantran012080@gmail.com>
2024-10-11 21:50:49 +0700

    get posts and get my post api routes

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -29,3 +29,67 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMyPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    // Lấy bài viết của chính người dùng dựa trên userId từ token
+    const myPosts = await Post.find({ userId: req.user.id })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    if (myPosts.length === 0) {
+      return res.status(200).json({ message: "There's no post" });
+    }
+
+    const totalMyPosts = await Post.countDocuments({ userId: req.user.id });
+
+    res.status(200).json({ myPosts, totalMyPosts });
+  } catch (error) {
+    next(error);
+  }
+};
