d62cc1fb0fc769295802108103055fbee6af5009
hansoo280102 <tantran012080@gmail.com>
2024-10-14 23:36:40 +0700

    1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -30,17 +30,6 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, "Username must be between 7 and 20 characters!")
       );
     }
-    if (req.body.username.includes(" ")) {
-      return next(errorHandler(400, "Username cannot contain spaces!"));
-    }
-    if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, "Username must be lowercase!"));
-    }
-    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-      return next(
-        errorHandler(400, "Username can only contain alphanumeric characters!")
-      );
-    }
   }
 
   // Kiểm tra role
